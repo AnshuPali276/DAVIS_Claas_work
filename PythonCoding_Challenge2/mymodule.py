439be4e6f726ac0f3b66302e44f1c3558df2568a
AnshuPali276 <anshupali0676@gmail.com>
2026-04-09 11:10:58 +0530

    Created using Colab

---
+++ PythonCoding_Challenge2/mymodule.py
@@ -0,0 +1,43 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNTJQMr5V8RiDYq+xFtuEzi",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/AnshuPali276/DAVIS_Claas_work/blob/main/PythonCoding_Challenge2/mymodule.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 72,
+      "metadata": {
+        "id": "8MIsOo8vXkNS"
+      },
+      "outputs": [],
+      "source": [
+        "# Create Module and Import\n",
+        "def add(a, b):\n",
+        "    return a + b"
+      ]
+    }
+  ]
+}
